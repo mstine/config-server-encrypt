beb2338ca1f88b2a0b7f92f75ee9745bb3e1be7a
Matt Stine <mstine@gopivotal.com>
2016-01-05 08:46:25 -0600

    initial commit

---
+++ src/test/java/io/pivotal/spring/cloud/configServerEncrypt/ConfigServerEncryptApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.spring.cloud.configServerEncrypt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigServerEncryptApplication.class)
+public class ConfigServerEncryptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
